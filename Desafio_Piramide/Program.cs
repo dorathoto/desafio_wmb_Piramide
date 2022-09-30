1f3e866a2851f5ad7968a5e6f8bd7abee512eca0
Leonardo Dorathoto <leonardo.dorathoto@gmail.com>
2022-09-30 10:31:43 -0300

    add paulo

--- Desafio_Piramide/Program.cs
+++ Desafio_Piramide/Program.cs
@@ -4,10 +4,16 @@ internal class Program
 {
     static void Main(string[] args)
     {
+
+
+        Console.WriteLine("Digite quantos níveis vai ter o seu triângulo");
+        var nivelUser = Convert.ToInt32(Console.ReadLine());
+
+
+        //By Alec
+        int linhas = nivelUser;
         int cerca = 1;
         int contador;
-        Console.WriteLine("Digite um valor: ");
-        int linhas = Convert.ToInt32(Console.ReadLine());
         contador = linhas;
 
         if (contador > 3 && contador <= 8)
@@ -31,6 +37,44 @@ internal class Program
         {
             Console.WriteLine("As linhas devem estar maior que 3 e menor ou igual a 8!");
         }
+
+
+        //By Paulo
+
+        int n, a;
+        n = nivelUser;
+        string[,] nivel = new string[n, n];
+        a = n - 1;
+
+        if (n < 3 || n > 8)
+        {
+            Console.WriteLine("ERRO: Digite um valor entre 3 e 8");
+        }
+        else
+        {
+
+            for (int l = 0; l < n; l++)
+            {
+                for (int c = 0; c < n; c++)
+                {
+                    if (c >= a)
+                    {
+                        Console.Write("#");
+
+                    }
+                    else
+                    {
+                        Console.Write(' ');
+                    }
+
+                }
+                a = a - 1;
+                Console.WriteLine();
+            }
+
+        }
+
+
         Console.ReadLine();
     }
 }
